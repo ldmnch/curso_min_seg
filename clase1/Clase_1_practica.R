e0d491d218f96462f496dc4126db2895112c3e5a
ldmnch <laiadomenechburin@gmail.com>
2022-03-25 14:02:07 -0300

    Primer commit

---
+++ clase1/Clase_1_practica.R
@@ -0,0 +1,23 @@
+### EJERCICIOS DE PRÁCTICA
+
+# 1. Crear un objeto llamado "primer_objeto" que defina el resultado de la multiplicación 6*4
+
+# 2. Crear un vector llamado "ingresos" que contenga los valores: 45000, 70000, 90000, 25000
+
+# 3. Modificar el valor del tercer elemento del vector ingresos por otro número
+
+# 4. Crear un vector de tipo character que tenga la misma cantidad de valores que el vector "ingresos"
+
+# 5. Crear un factor que se llame "nivel_educativo", que contenga los valores: "Hasta primario completo", "Hasta secundario completo" y "Hasta terciario/universitario completo" y tenga la misma cantidad de registros que el vector "ingresos".
+# Asígnele los niveles que le parezcan correspondientes. 
+
+# 6a. Cree un objeto dataframe combinando los vectores creados previamente. 
+# 6b. ¿Qué funciones puedo usar para explorar brevemente el dataframe? Úselas y describa que realizan.
+
+# 7. Calcule la media de ingresos para el dataframe y almacénela en un objeto llamado "media_ingresos". 
+
+# 8. ¿Cómo puedo acceder al valor que está en la primera fila de la segunda columna del dataframe? 
+
+# 9. Cree una tabla de frecuencias para la columna "nivel_educativo". 
+
+# 10. Borre todos los objetos creados. 
---
+++ clase2/Clase_2_practica.R
@@ -0,0 +1,15 @@
+### 1. Instalen los paquetes haven y openxlsx
+
+### 2. Abran el .RProj de la carpeta 
+
+### 3. Importen la base de homicidios dolosos detallando la ruta desde el punto de partida del proyecto:
+### a. En formato .dta
+### b. En formato .xlsx
+
+### 4. ¿Cómo es la distribución de frecuencias del sexo de las víctimas? ¿Y de la identidad de género? 
+
+### 5. Creen un objeto que contenga las primeras 500 filas de la base de datos. 
+
+### 6. Cree un objeto donde estén únicamente aquellas filas que provincia == "BUENOS AIRES"
+
+### 7. Guardar esta última base de datos en formato .RDS
