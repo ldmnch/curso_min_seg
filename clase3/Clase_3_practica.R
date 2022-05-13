982380a1bd34148cb073bd54b643bf931cbca9ab
ldmnch <laiadomenechburin@gmail.com>
2022-05-13 11:06:51 -0300

    Practica resuelta

--- clase3/Clase_3_practica.R
+++ clase3/Clase_3_practica.R
@@ -1,14 +1,33 @@
 ### 1a. Importar tidyverse
+
+library(tidyverse)
+
 ### 1b. Importar la base de datos de PUFEAF.
 
+base_pufeaf <- read.csv("./data/Base_PUFEAF.csv")
+
 ### 2. Crear un objeto que contenga únicamente 4 variables a elección de la base de datos. 
+select_4elementos <- base_pufeaf %>% 
+        select(Provincia, Aglomerado, Partido_Municipio, HORA)
 
 ### 3. Crear un objeto que contenga unicamente un tipo de fuerza de eleccion y 3 provincias a eleccion. 
-
-### 4. Crear una nueva columna llamada "hubo_fallecidos" que indique si hubo algún fallecido (sea civil interviniente, tercero, víctima o
+nombre_fuerza_provincia <- base_pufeaf %>% 
+        filter(Provincia %in% c("Chubut","Rio Negro","Cordoba") 
+               & Tipo_fuerza == "PFA")
+### 4. Crear una nueva columna llamada "hubo_fallecidos" que indique si hubo algún fallecido
+###(sea civil interviniente, tercero, víctima o
 ###policial).
 
-### 5. Crear una nueva columna llamada "tipo_fallecidos" que indique si el fallecido fue civil interviniente, tercero, víctima o policial.
+base_pufeaf <- base_pufeaf %>% mutate(hubo_fallecidos = case_when(
+        Personal_Fallecido > 0 | Civil_Interviniente_Fallecido > 0 |
+        Civil_Tercero_Fallecido > 0 | Civil_Victima_Fallecido > 0 ~ "Si",
+        TRUE ~ "No"
+))
+table(base_pufeaf$hubo_fallecidos)
+### 5. Crear una nueva columna llamada "tipo_fallecidos" que indique si 
+###el fallecido fue civil interviniente, tercero, víctima o policial.
+
+
 
 ### 5. Eliminar las últimas dos columnas del dataframe. 
 
